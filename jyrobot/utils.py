a10480ac8ffbc5a39648813c63fffe9165d29419
Douglas Blank <doug.blank@gmail.com>
2020-12-17 07:38:35 -0800

    Added color names

--- jyrobot/utils.py
+++ jyrobot/utils.py
@@ -10,6 +10,8 @@
 
 from ipywidgets import Layout
 
+from .color_data import COLORS
+
 # from ipylab import JupyterFrontEnd
 
 # def remove_canvases():
@@ -35,6 +37,17 @@ def get_canvas(config, width, height, scale=1.0, gc=None):
 
 class Color:
     def __init__(self, red, green=None, blue=None, alpha=None):
+        self.name = None
+        if isinstance(red, str):
+            if red.startswith("#"):
+                # encoded hex color
+                red, green, blue, alpha = self.hex_to_rgba(red)
+            else:
+                # color name
+                self.name = red
+                hex_string = COLORS.get(red, "#00000000")
+                red, green, blue, alpha = self.hex_to_rgba(hex_string)
+
         self.red = red
         if green is not None:
             self.green = green
@@ -49,11 +62,30 @@ class Color:
         else:
             self.alpha = 255
 
+    def hex_to_rgba(self, hex_string):
+        r_hex = hex_string[1:3]
+        g_hex = hex_string[3:5]
+        b_hex = hex_string[5:7]
+        if len(hex_string) > 7:
+            a_hex = hex_string[7:9]
+        else:
+            a_hex = "FF"
+        return int(r_hex, 16), int(g_hex, 16), int(b_hex, 16), int(a_hex, 16)
+
     def __str__(self):
-        return "#%02X%02X%02X%02X" % self.to_tuple()
+        if self.name is not None:
+            return self.name
+        else:
+            return self.to_hexcode()
+
+    def __repr__(self):
+        return "<Color%s>" % (self.to_tuple(),)
 
     def to_tuple(self):
         return (int(self.red), int(self.green), int(self.blue), int(self.alpha))
+
+    def to_hexcode(self):
+        return "#%02X%02X%02X%02X" % self.to_tuple()
 
 
 class Point:
