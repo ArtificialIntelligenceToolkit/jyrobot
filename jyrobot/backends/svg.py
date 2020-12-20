41dcba6eaf4c89d750f93cb913ae126d31a3e0ea
Douglas Blank <doug.blank@gmail.com>
2020-12-19 21:18:29 -0800

    SVG close enough

--- jyrobot/backends/svg.py
+++ jyrobot/backends/svg.py
@@ -12,6 +12,7 @@ import math
 
 from svgwrite import Drawing
 
+from ..utils import Color
 from .base import Backend
 
 
@@ -31,38 +32,88 @@ class SVGBackend(Backend):
         self.stack.append(dwg)
 
     def set_stroke_style(self, color):
-        self.stroke_style = color.rgb()
+        self.stroke_style = color
+        self.stroke_style_color = Color(0, 0, 0, 255)
 
     def set_fill_style(self, color):
         self.fill_style = color.rgb()
+        self.fill_style_color = color
+
+    def fill_opacity(self):
+        alpha = self.fill_style_color.alpha
+        if alpha != 255:
+            return round(alpha / 255, 2)
+
+    def stroke_opacity(self):
+        alpha = self.stroke_style_color.alpha
+        if alpha != 255:
+            return round(alpha / 255, 2)
 
     def get_style(self, *items):
+        fo = self.fill_opacity()
+        so = self.stroke_opacity()
         map = {
             "fill": ("fill:%s" % self.fill_style) if self.fill_style else None,
             "stroke-width": "stroke-width:%s" % self.line_width,
             "stroke": ("stroke:%s" % self.stroke_style) if self.stroke_style else None,
+            "fill-opacity": ("fill-opacity:%s" % fo) if fo is not None else None,
+            "stroke-opacity": ("stroke-opacity:%s" % so) if so is not None else None,
         }
         styles = []
         for item in items:
-            styles.append(map[item])
+            v = map[item]
+            if v:
+                styles.append(v)
         style = ";".join(styles)
         return style
 
     def arc(self, x, y, radius, startAngle, endAngle):
         def polarToCartesian(centerX, centerY, radius, angleInRadians):
             return [
-                centerX + (radius * math.cos(angleInRadians)),
-                centerY + (radius * math.sin(angleInRadians)),
+                round(centerX + (radius * math.cos(angleInRadians)), 2),
+                round(centerY + (radius * math.sin(angleInRadians)), 2),
             ]
 
         start = polarToCartesian(x, y, radius, endAngle)
         end = polarToCartesian(x, y, radius, startAngle)
 
-        path = ["M", start[0], start[1], "A", radius, radius, 0, 0, 0, end[0], end[1]]
+        if len(self.points) > 0:
+            path = [
+                "M",
+                start[0],
+                start[1],
+                "A",
+                round(radius, 2),
+                round(radius, 2),
+                0,
+                0,
+                0,
+                end[0],
+                end[1],
+                "L",
+                self.points[-1][0],
+                self.points[-1][1],
+            ]
+            styles = ["fill", "fill-opacity"]
+        else:
+            path = [
+                "M",
+                start[0],
+                start[1],
+                "A",
+                round(radius, 2),
+                round(radius, 2),
+                0,
+                0,
+                0,
+                end[0],
+                end[1],
+            ]
+            styles = ["stroke", "stroke-width"]
         path = [str(item) for item in path]
         d = " ".join(path)
         dwg = self.stack[-1]
-        style = self.get_style("fill", "stroke", "stroke-width")
+        style = self.get_style(*styles)
         dwg.add(self.stack[0].path(d=d, style=style))
 
     def get_image_data(self):
@@ -79,7 +130,11 @@ class SVGBackend(Backend):
     def fill_rect(self, x, y, width, height):
         style = self.get_style("fill")
         self.stack[-1].add(
-            self.stack[0].rect(insert=(x, y), size=(width, height), style=style)
+            self.stack[0].rect(
+                insert=(round(x, 2), round(y, 2)),
+                size=(round(width, 2), round(height, 2)),
+                style=style,
+            )
         )
 
     def fill(self):
@@ -91,7 +146,7 @@ class SVGBackend(Backend):
 
     def stroke(self):
         if len(self.points) >= 2:
-            style = self.get_style("stroke", "stroke-width")
+            style = self.get_style("stroke", "stroke-width", "stroke-opacity")
             dwg = self.stack[-1]
             last = self.points[0]
             for point in self.points[1:]:
@@ -100,10 +155,10 @@ class SVGBackend(Backend):
             self.points.clear()
 
     def move_to(self, x, y):
-        self.points = [(x, y)]
+        self.points = [(round(x, 2), round(y, 2))]
 
     def line_to(self, x, y):
-        self.points.append([x, y])
+        self.points.append([round(x, 2), round(y, 2)])
 
     def save(self):
         dwg = self.stack[0]
@@ -136,8 +191,14 @@ class SVGBackend(Backend):
 
     def ellipse(self, x, y, radiusX, radiusY, a, b, angle):
         dwg = self.stack[-1]
-        style = self.get_style("fill", "stroke", "stroke-width")
-        dwg.add(self.stack[0].ellipse(center=(x, y), r=(radiusX, radiusY), style=style))
+        style = self.get_style("fill")
+        dwg.add(
+            self.stack[0].ellipse(
+                center=(round(x, 2), round(y, 2)),
+                r=(round(radiusX, 2), round(radiusY, 2)),
+                style=style,
+            )
+        )
 
     def put_image_data(self, scaled, x, y):
         pass
