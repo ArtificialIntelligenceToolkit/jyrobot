0ad2b95099d87a49c933c0ccc1521e1dde724992
Douglas Blank <doug.blank@gmail.com>
2021-01-30 15:28:26 -0800

    Version 0.6.18

--- jyrobot/_version.py
+++ jyrobot/_version.py
@@ -2,11 +2,11 @@
 # *************************************
 # jyrobot: Python robot simulator
 #
-# Copyright (c) 2020 Calysto Developers
+# Copyright (c) 2021 Calysto Developers
 #
 # https://github.com/Calysto/jyrobot
 #
 # *************************************
 
-version_info = (0, 6, 17)
+version_info = (0, 6, 18)
 __version__ = ".".join(map(str, version_info))
